65100ea62a104eba52bdb192438f4bcf97ad84c6
mohammademon393 <mdemon357ep@gmail.com>
2025-09-20 11:33:32 +0600

    add to cart btn and update others component

--- src/component/Product/Product.jsx
+++ src/component/Product/Product.jsx
@@ -4,11 +4,16 @@ const Product = ({ product }) => {
     console.log(product);
     
   return (
-    <div className='card-container'>
+    <div className="card-container2">
       <div className="card">
-        <img className='img' src={product.image} alt="" />
-        <h2>{product.title}</h2>
-        <p>{product.description}</p>
+        <div className="img-container">
+          <img className="img" src={product.image} alt="" />
+        </div>
+        <h3>{product.title}</h3>
+        <p>Price: {product.price} </p>
+        <p>category: {product.category}</p>
+        <p>rating: {product.rating.rate}</p>
+        <button className='btn'>Add to cart</button>
       </div>
     </div>
   );
--- src/component/Products/Products.jsx
+++ src/component/Products/Products.jsx
@@ -1,16 +1,20 @@
 import { use } from "react";
 import Product from "../Product/Product";
+import './Products.css';
 
 const Products = ({ productsData }) => {
     const products = use(productsData);
     // console.log(products);
     
   return (
-    <div className="card-container">
+    <div>
       <h1>Products...</h1>
-      {products.map((product) => (
-        <Product key={product.id} product={product}></Product>
-      ))}
+
+      <div className="card-container">
+        {products.map((product) => (
+          <Product key={product.id} product={product}></Product>
+        ))}
+      </div>
     </div>
   );
 };
