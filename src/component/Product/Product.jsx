faa3a3d83b1c62902ec34cb618d17d503156a577
mohammademon393 <mdemon357ep@gmail.com>
2025-09-20 11:05:52 +0600

    product data show the display

--- src/component/Product/Product.jsx
+++ src/component/Product/Product.jsx
@@ -1,10 +1,16 @@
 
-const Product = () => {
-    return (
-        <div>
-            <h3>product is here</h3>
-        </div>
-    );
+const Product = ({ product }) => {
+    console.log(product);
+    
+  return (
+    <div>
+      <div className="card">
+        <img src={product.image} alt="" />
+        <h2>{product.title}</h2>
+        <p>{product.description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
--- src/component/Products/Products.jsx
+++ src/component/Products/Products.jsx
@@ -1,12 +1,16 @@
 import { use } from "react";
+import Product from "../Product/Product";
 
 const Products = ({ productsData }) => {
     const products = use(productsData);
-    console.log(products);
+    // console.log(products);
     
   return (
     <div>
       <h1>Products...</h1>
+      {products.map((product) => (
+        <Product key={product.id} product={product}></Product>
+      ))}
     </div>
   );
 };
